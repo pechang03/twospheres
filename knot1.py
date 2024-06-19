9c8995518b36f12cbddf8905cd34de0af3825a01
pechang03 <peter.shaw.cs@gmail.com>
2024-06-19 16:06:39 +0800

    Removed axis limit settings from 3D plot in knot1.py.

--- knot1.py
+++ knot1.py
@@ -3,19 +3,18 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 t = np.linspace(0, 2 * np.pi, 1000)
- x = np.sin(t) + 2 * np.sin(2 * t)
- y = np.cos(t) - 2 * np.cos(2 * t)
- z = -np.sin(3 * t)
+x = np.sin(t) + 2 * np.sin(2 * t)
+y = np.cos(t) - 2 * np.cos(2 * t)
+z = -np.sin(3 * t)
 
- fig = plt.figure()
- ax = fig.add_subplot(111, projection="3d")
- ax.plot(x, y, z, color="b", alpha=0.5)
- ax.set_xlim(-4, 4)
- ax.set_ylim(-4, 4)
- ax.set_zlim(-4, 4)
- ax.set_xlabel("X")
- ax.set_ylabel("Y")
- ax.set_zlabel("Z")
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.plot(x, y, z, color="b", alpha=0.5)
+# ax.set_xlim(-4, 4)
+# ax.set_ylim(-4, 4)
+# ax.set_zlim(-4, 4)
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
 
- plt.show()
-
+plt.show()
