77c7b27792fa8060fa2ae5cad4fc8eb5279b73ac
pechang03 <peter.shaw.cs@gmail.com>
2024-06-19 16:00:39 +0800

    aider: Removed axis limits and labels configuration from knot1.py.

--- knot1.py
+++ knot1.py
@@ -3,13 +3,3 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# Set the axis limits and labels
-ax.set_xlim(-3, 3)
-ax.set_ylim(-3, 3)
-ax.set_zlim(-3, 3)
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Z")
-
-# Show the plot
-plt.show()
