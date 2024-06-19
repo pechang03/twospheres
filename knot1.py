913ed97fdf8b9921a9f098927bbedcd850158765
pechang03 <peter.shaw.cs@gmail.com>
2024-06-19 16:05:24 +0800

    Fixed knot1.py to use consistent whitespace in the assignment of variable t.

--- knot1.py
+++ knot1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
- t = np.linspace(0, 2 * np.pi, 1000)
+t = np.linspace(0, 2 * np.pi, 1000)
  x = np.sin(t) + 2 * np.sin(2 * t)
  y = np.cos(t) - 2 * np.cos(2 * t)
  z = -np.sin(3 * t)
