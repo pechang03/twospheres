66ccfe43a38e491f03257c2acdf1e18aa8b883e9
pechang03 <peter.shaw.cs@gmail.com>
2024-06-26 09:20:57 +0800

    Refactored overlay graph plotting to use a single figure and removed redundant code.

--- overlay_graph.py
+++ overlay_graph.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from two_spheres_g import plot_spheres  # Assuming the function to plot spheres is in this file
 
-# Create the geometric graph with seed for reproducibility
+# Use seed when creating the graph for reproducibility
 G = nx.random_geometric_graph(200, 0.125, seed=896803)
-# Get positions of nodes
+# position is stored as node attribute data for random_geometric_graph
 pos = nx.get_node_attributes(G, "pos")
 
-# Find node near the center (0.5, 0.5)
+# find node near center (0.5,0.5)
 dmin = 1
 ncenter = 0
 for n in pos:
@@ -17,14 +17,11 @@ for n in pos:
         ncenter = n
         dmin = d
 
-# Color nodes by path length from node near the center
+# color by path length from node near center
 p = dict(nx.single_source_shortest_path_length(G, ncenter))
 
-# Plot spheres from two_spheres_g.py
-fig, ax = plt.subplots()
-plot_spheres(ax)  # Assuming this function takes an axis as argument
-
-# Overlay the geometric graph nodes
+plt.figure(figsize=(8, 8))
+nx.draw_networkx_edges(G, pos, alpha=0.4)
 nx.draw_networkx_nodes(
     G,
     pos,
@@ -32,15 +29,6 @@ nx.draw_networkx_nodes(
     node_size=80,
     node_color=list(p.values()),
     cmap=plt.cm.Reds_r,
-    ax=ax,  # Draw on the same axis as spheres
 )
 
-# Draw edges of the geometric graph
-nx.draw_networkx_edges(G, pos, alpha=0.4, ax=ax)
-
-# Set plot limits and remove axes
-plt.xlim(-0.05, 1.05)
-plt.ylim(-0.05, 1.05)
-plt.axis("off")
-
-plt.show()
+plt.pos, ax=0.4)
